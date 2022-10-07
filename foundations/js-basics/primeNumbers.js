cc139d405e66fca4cb27ff64062478ee16e75fae
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-08 00:20:33 +1100

    Debug getPrimes and implement working checkPrime function

--- foundations/js-basics/primeNumbers.js
+++ foundations/js-basics/primeNumbers.js
@@ -1,24 +1,38 @@
-function getPrimes(n) {
-  n = Math.floor(n);
-  
-  if (n <= 1) {
-    console.log("No primes")
-    return;
-  }
+function checkPrime(n) {
+  if (n < 2) {
+    return false;
+  } else {
+    let divisibleCount = 0;
+    
+    for (i = 2; i <= n; i++) {
+      if (n % i == 0) {
+        divisibleCount += 1;
+      }
+    }
 
-  for (let i = 2; i <= n; i++) {
-    if (checkPrime(n)) {
-      console.log(i + " ")
+    if (divisibleCount == 1) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
 
-function checkPrime(n) {
-  if (n == 2) {
-    return true;
+function getPrimes(n) {
+  n = Math.floor(n);
+  
+  if (n < 2) {
+    console.log("No primes")
+    return;
   } else {
-
-  }
+    let primesList = [];
+    for (let i = 2; i <= n; i++) {
+      if (checkPrime(i)) {
+        primesList.push(i);
+      }
+    }
+    console.log(primesList);
+    }
 }
 
-getPrimes(13)
+getPrimes(100)
